08f70b995b2ea84c5ba8fe24613309ced1106936
AndrejG231 <andrej.germic@gmail.com>
2021-05-29 16:28:08 +0200

    Updated user practice stats redux logic

--- client/src/redux/actions/setUsersPracticeStat.ts
+++ client/src/redux/actions/setUsersPracticeStat.ts
@@ -1,13 +1,17 @@
-import {
-  action,
-  userPracticeStats,
-} from "../../types/types_redux/userPracticeStatsT";
+import { action } from "../../types/types_redux/userPracticeStatsT";
+import { leaderboardItem } from "../../types";
 
-const setPracticeUserStats = (stat: userPracticeStats): action => {
+const setUserPracticeStats = (
+  category: string,
+  index: number,
+  stat: leaderboardItem
+): action => {
   return {
-    type: "userStats/set",
-    stat: stat,
+    type: "userPracticeStats/update",
+    item: stat,
+    category: category,
+    practiceIndex: index,
   };
 };
 
-export default setPracticeUserStats;
+export default setUserPracticeStats;
--- client/src/redux/reducers/userPracticeStats.ts
+++ client/src/redux/reducers/userPracticeStats.ts
@@ -2,8 +2,22 @@ import { reducer } from "../../types/types_redux/userPracticeStatsT";
 
 const userPracticeStats: reducer = (state = {}, action) => {
   switch (action.type) {
-    case "userStats/set":
-      return { ...state, ...action.stat };
+    case "userPracticeStats/update":
+      return {
+        ...state,
+        [action.category]: {
+          ...state[action.category],
+          [action.practiceIndex]: action.item,
+        },
+      };
+    case "userPracticeStats/remove":
+      return {
+        ...state,
+        [action.category]: {
+          ...state[action.category],
+          [action.practiceIndex]: null,
+        },
+      };
     default:
       return state;
   }
