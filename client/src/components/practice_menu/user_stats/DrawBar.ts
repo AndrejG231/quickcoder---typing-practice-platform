23045b2633874b175028b64adb322871e91345e9
AndrejG231 <andrej.germic@gmail.com>
2021-06-03 08:22:50 +0200

    Exported color getter function from stats draw bar

--- client/src/components/practice_menu/user_stats/DrawBar.ts
+++ client/src/components/practice_menu/user_stats/DrawBar.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { statColorSwitch } from "../../../utilites";
 
 interface props {
   width: number;
@@ -6,22 +7,7 @@ interface props {
 }
 
 const DrawBar = styled.div.attrs<props>(({ width, theme }) => {
-  let color;
-  if (width < 5) {
-    color = theme.colors.b5;
-  } else if (width < 10) {
-    color = "#801c1c";
-  } else if (width < 25) {
-    color = "#915411";
-  } else if (width < 50) {
-    color = "#8c7918";
-  } else if (width < 75) {
-    color = "#549400";
-  } else if (width < 100) {
-    color = "#45700c";
-  } else {
-    color = "green";
-  }
+  const color = statColorSwitch(width);
 
   return {
     style: {
---
+++ client/src/utilites/statColorSwitch.ts
@@ -0,0 +1,19 @@
+const statColorSwitch = (width: number): string => {
+  if (width < 5) {
+    return "black";
+  } else if (width < 10) {
+    return "#801c1c";
+  } else if (width < 25) {
+    return "#915411";
+  } else if (width < 50) {
+    return "#8c7918";
+  } else if (width < 75) {
+    return "#549400";
+  } else if (width < 100) {
+    return "#45700c";
+  } else {
+    return "green";
+  }
+};
+
+export default statColorSwitch;
