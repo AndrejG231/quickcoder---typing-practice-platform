ba37892e244a55c4c67b00fc60b783a53d4d464c
AndrejG <andrej.germic@gmail.com>
2021-07-06 21:00:07 +0200

    Added webpack config

---
+++ client/config/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "..", "./src/index.tsx"),
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env",
+                [
+                  "@babel/preset-react",
+                  {
+                    runtime: "automatic",
+                  },
+                  "@babel/preset-typescript",
+                ],
+              ],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, "..", ".docs"),
+    filename: "bundle.js",
+  },
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "..", "./public/index.html"),
+    }),
+  ],
+};
