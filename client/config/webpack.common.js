deba8b3f60595276be69fee9efcf2e00ef5feeb9
AndrejG <andrej.germic@gmail.com>
2021-07-07 20:30:56 +0200

    Added webpack config

---
+++ client/config/webpack.common.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const mode = process.env.mode;
+
+module.exports = {
+  entry: path.resolve(__dirname, "..", "./src/index.tsx"),
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  devServer: { contentBase: path.join(__dirname, "..", "./src") },
+  module: {
+    rules: [
+      // JavaScript loader
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env",
+                "@babel/preset-react",
+                "@babel/preset-typescript",
+              ],
+              plugins: [
+                mode === "dev" && require.resolve("react-refresh/babel"),
+              ],
+            },
+          },
+        ],
+      },
+      // Typescript loader
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"],
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, "..", ".docs"),
+    filename: "bundle.js",
+  },
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "..", "./public/index.html"),
+    }),
+  ],
+};
--- client/config/webpack.config.js
+++ client/config/webpack.config.js
@@ -1,45 +1,8 @@
-const path = require("path");
-const htmlWebpackPlugin = require("html-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require("webpack-merge");
+const commonConfig = require("./webpack.common.js");
 
-module.exports = {
-  entry: path.resolve(__dirname, "..", "./src/index.tsx"),
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: "babel-loader",
-            options: {
-              presets: [
-                "@babel/preset-env",
-                [
-                  "@babel/preset-react",
-                  {
-                    runtime: "automatic",
-                  },
-                  "@babel/preset-typescript",
-                ],
-              ],
-            },
-          },
-        ],
-      },
-    ],
-  },
-  output: {
-    path: path.resolve(__dirname, "..", ".docs"),
-    filename: "bundle.js",
-  },
-  mode: "development",
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "..", "./public/index.html"),
-    }),
-  ],
+module.exports = (enviroment) => {
+  const { mode } = enviroment;
+  const modeConfig = require(`./webpack.${mode}.js`);
+  return merge(commonConfig, modeConfig);
 };
