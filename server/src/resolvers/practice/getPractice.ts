db1f7ce4474bbdd591bfbf468746dfa513c08424
AndrejG <andrej.germic@gmail.com>
2021-07-13 12:05:00 +0200

    Disabled typing test fetching

--- server/src/resolvers/practice/getPractice.ts
+++ server/src/resolvers/practice/getPractice.ts
@@ -4,7 +4,7 @@ import { generateResponse } from "../../utilities/";
 const getPractice = async (id: number) => {
   const practice = await Practices.findOne({ id: id });
 
-  if (practice) {
+  if (practice && practice.category !== "test") {
     return {
       result: generateResponse(true, "getPracticesObject_practice_received"),
       practice: practice,
