0f18db72722b44515f5f5a3336f821742c9001ea
AndrejG <andrej.germic@gmail.com>
2021-07-26 13:27:38 +0200

    Fixed invisible navbar on different pages

--- client/src/components/NavBar.ts
+++ client/src/components/NavBar.ts
@@ -6,7 +6,12 @@ interface props {
 
 const NavBar = styled.div<props>`
   ${({ theme, isOnScreen }) => `
- ${!isOnScreen ? "transform: translateY(-200px)" : ""};
+ ${
+  //  Type check to disable navbar hiding when animation is not set up on specific page => DISABLE WHEN FINISHED
+   !isOnScreen && typeof isOnScreen === "boolean"
+     ? "transform: translateY(-200px)"
+     : ""
+ };
  transition: 0.3s all linear;
   display: flex;
   justify-content: space-between;
